c7f3a7691794084812314f3b37713125d9b59f1c
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-05-13 11:22:22 -0700

    Imported useRestaurants to the RestaurantsContainer and used it to pass information to the rest of the component.

--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -1,6 +1,12 @@
 import Restaurant from "./Restaurant";
 
+// Importing a specific piece of information from the Context
+import { useRestaurants } from "../context/RestaurantContext";
+
 function RestaurantsContainer() {
+
+  //Saving the import to a variable so that it can be accessed by the rest of the code
+  const {restaurants} = useRestaurants();
   return (
     <div className="restaurantContainer">
       {restaurants.map((restaurant) => (
